35a37b04ac919807180229cf706a66132e53702e
Abu Talha <talha007abu@gmail.com>
2025-02-01 02:50:35 +0600

    first commit

---
+++ sum.py
@@ -0,0 +1,24 @@
+num1 = 10 
+num2 = 15 
+
+result = num1 + num2 
+
+print("Sum is = " , result)
+
+# kontar type ki janar jonno 
+
+print(type(num1))
+
+
+
+ # stirng and int add
+
+# a = 5 
+# b = "s"
+
+# sum = a + b ; # not poosible ;
+
+# print(sum)
+
+
+
